405538e4743fb203a7ad24e0c70f2561684b83f2
Johnjoe Landers <johnjoelanders@outlook.com>
2016-11-05 16:11:30 +0000

    In Book.java i created a constructure to create a book and created a getter method for title and a getter and setter for get number of copies. In library.java first i implemented subject then i created a list to store observers i created a list to store a list of books. in libarary i added a few books to the list. I have a method called buyBook which gets passed a string which is the title and search the list for a book with the similar title if there is one with a similar title it decrease the number of copies by one the it checks if the number of books where less than 2 and it implements the method called notifyObservers.

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Book{
+    private List<Observer> observers = new ArrayList<Observer>();
+    private String title;
+    private String author;
+    private String publisher;
+    private double price;
+    private boolean isHardback;
+    private boolean isPictures;
+    private int numOfPages;
+    private long isbn;
+    private int noOfCopies;
 
+    public Book(String title, String author, String publisher, double price, boolean isHardback, boolean isPictures, int numOfPages, long isbn, int noOfCopies) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+        this.isHardback = isHardback;
+        this.isPictures = isPictures;
+        this.numOfPages = numOfPages;
+        this.isbn = isbn;
+        this.noOfCopies = noOfCopies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNoOfCopies() {
+        return noOfCopies;
+    }
+
+    public void setNoOfCopies(int noOfCopies) {
+        this.noOfCopies = noOfCopies;
+    }
 }
--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,55 @@
-public class Library implements Subject{
+import java.util.ArrayList;
+import java.util.List;
 
+public class Library implements Subject{
+    private List<Observer> observers = new ArrayList<Observer>();
+    public List<Book> books = new ArrayList<Book>();
+    String updateBookTitle;
+    public Library(){
+        Book b1 = new Book("Harry Potter and the Sorcerer's Stone","J.K. Rowling","Arthur A. Levine Books",12.80,true,true,320,143955493L,5);
+        Book b2 = new Book("Harry Potter and the Prisoner of Azkaban","J.K. Rowling","Scholastic",15.30,true,false,435,143955493L,7);
+        Book b3 = new Book("Harry Potter and the Chamber of Secrets","J.K. Rowling","Scholastic",11.00,true,false,352,143955493L,3);
+        Book b4 = new Book("Harry Potter and the Deathly Hallows","J.K. Rowling","Scholastic",9.80,true,false,784,143955493L,4);
+        Book b5 = new Book("Test","J.K. Rowling","Scholastic",9.80,true,false,784,143955493L,3);
+
+        books.add(b1);
+        books.add(b2);
+        books.add(b3);
+        books.add(b4);
+        books.add(b5);
+
+    }
+    public void buyBook(String title){
+        for(Book book : books){
+            if(book.getTitle() == title){
+                book.setNoOfCopies(book.getNoOfCopies()-1);
+                if(book.getNoOfCopies() <= 2){
+                    updateBookTitle = title;
+                    notifyObservers();
+                }
+            }
+        }
+    }
+    public void addBook(String title){
+        for (Book book :books){
+            if(book.getTitle() == title){
+                int numOfBooks1 = book.getNoOfCopies();
+                System.out.println("Number of books before : "+numOfBooks1);
+                book.setNoOfCopies(book.getNoOfCopies()+1);
+                int numOfBooks = book.getNoOfCopies();
+                System.out.println("Number of books after: "+numOfBooks);
+            }
+        }
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
 }
 
